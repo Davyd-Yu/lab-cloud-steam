9a94ab40e630bc08e548cd5963b450a71e531026
Davyd_Yukhymchuk <davyd.yukhymchuk.ir.2022@lpnu.ua>
2025-12-12 13:06:43 +0200

    Moved swagger docs to routes

--- app/my_project/games/controller/game_controller.py
+++ app/my_project/games/controller/game_controller.py
@@ -5,42 +5,14 @@ service = GameService()
 
 class GameController:
     def get_all_games(self):
-        """
-        Отримати список ігор
-        ---
-        tags:
-          - Games
-        responses:
-          200:
-            description: Повертає JSON зі списком ігор
-        """
         return jsonify(service.get_all())
 
     def create_game(self):
-        """
-        Додати нову гру
-        ---
-        tags:
-          - Games
-        parameters:
-          - name: body
-            in: body
-            required: true
-            schema:
-              type: object
-              required:
-                - title
-                - price
-              properties:
-                title:
-                  type: string
-                  example: "Half-Life 3"
-                price:
-                  type: number
-                  example: 59.99
-        responses:
-          201:
-            description: Гру успішно створено
-        """
         service.create(request.json)
         return jsonify({"message": "Game created"}), 201
+
+    def get_game_details(self, game_id):
+        game = service.get_details(game_id)
+        if game:
+            return jsonify(game)
+        return jsonify({"error": "Not found"}), 404
--- app/my_project/games/route/game_route.py
+++ app/my_project/games/route/game_route.py
@@ -6,8 +6,62 @@ controller = GameController()
 
 @games_bp.route('/games', methods=['GET'])
 def get_games():
+    """
+    Отримати список ігор
+    ---
+    tags:
+      - Games
+    responses:
+      200:
+        description: Повертає JSON зі списком ігор
+    """
     return controller.get_all_games()
 
 @games_bp.route('/games', methods=['POST'])
 def add_game():
+    """
+    Додати нову гру
+    ---
+    tags:
+      - Games
+    parameters:
+      - name: body
+        in: body
+        required: true
+        schema:
+          type: object
+          required:
+            - title
+            - price
+          properties:
+            title:
+              type: string
+              example: "Half-Life 3"
+            price:
+              type: number
+              example: 59.99
+    responses:
+      201:
+        description: Гру успішно створено
+    """
     return controller.create_game()
+
+@games_bp.route('/games/<int:game_id>/details', methods=['GET'])
+def get_details(game_id):
+    """
+    Отримати деталі гри та категорії
+    ---
+    tags:
+      - Games
+    parameters:
+      - name: game_id
+        in: path
+        type: integer
+        required: true
+    responses:
+      200:
+        description: Детальна інформація про гру
+      404:
+        description: Гру не знайдено
+    """
+    return controller.get_game_details(game_id)
